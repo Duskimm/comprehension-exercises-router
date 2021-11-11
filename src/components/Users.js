998216ca1885113e99166f2021dcb53a26eea8d7
Douglas Toledo <Douglas Toledo - Duskimm>
2021-11-11 10:08:31 -0300

    requisito 09

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,19 +1,41 @@
-// arquivo Users.js
 import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
-    const { greetingsMessage } = this.props
+    const {
+      greetingMessage = 'Hi There',
+      match: { params: { id }
+    } } = this.props;
+
     return (
-      <div>
-        <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
-      </div>
+      <main>
+        <h2> Users </h2>
+        <p>
+        { `${greetingMessage} User ${id}` }, this is my awesome Users component
+        </p>
+      </main>
     );
   }
-};
+}
 
 export default Users;
+
+// // arquivo Users.js
+// import React, { Component } from 'react';
+
+// class Users extends Component {
+//   render() {
+//     const { greetingsMessage } = this.props
+//     return (
+//       <div>
+//         <h2>Users</h2>
+//         <p> { greetingsMessage }, My awesome Users component </p>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Users;
 
 // import React, { Component } from 'react';
 
